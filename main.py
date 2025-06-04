acedab314415ef6c79b679d31317ed4effdc2ad8
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-04 16:52:16 +0600

    Completed

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import random
 
-guess: int = random.randint(0,101)
+guess: int = random.randint(0, 101)
 gameOver: bool = False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while not gameOver:
-        user_input = input('Guess the number: ')
-        if user_input:
+        user_input = int(input("Guess the number: "))
+        if user_input == guess:
+            print("You won!")
             gameOver = True
-
+        elif user_input < guess:
+            print("Too low")
+        elif user_input > guess:
+            print("Too high")
